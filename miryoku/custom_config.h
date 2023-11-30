2bd9ff9399ee9ebf802543ab1dbe26810a95a620
4amVim <4429609+4amVim@users.noreply.github.com>
2023-11-30 07:46:07 +0530

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,8 +1,2 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
-
-#define MIRYOKU_ALPHAS_QWERTY
-#define MIRYOKU_TAP_QWERTY
-#define MIRYOKU_NAV_INVERTEDT
-#define MIRYOKU_CLIPBOARD_WIN
-#define MIRYOKU_MAPPING_EXTENDED_THUMBS
